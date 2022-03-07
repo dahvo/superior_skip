221bc77f517648e7764966e11915b7edb2890b32
dahvo <100693051+dahvo@users.noreply.github.com>
2022-03-06 22:19:00 -0500

    Initial commit

---
+++ superior_skip/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from superior_skip.users.models import User
+from superior_skip.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
